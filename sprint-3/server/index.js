d8da268134b4e1fdef62c122794c49ccf5373637
Jasper Leung <JasperLeung@Jaspers-MacBook-Pro.local>
2020-11-20 22:45:29 -0500

    post request completed

--- sprint-3/server/index.js
+++ sprint-3/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
+const {v4:uuidv4} = require('uuid')
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +29,27 @@ app.get("/videos/:id", (req, res) => {
     });
     res.status(200).json(response);
 });
+
+app.post("/videos", (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./data/database.json', "utf-8"));
+    console.log(req.body)
+    const video = {
+        id: uuidv4(),
+        title: req.body.title,
+        comments: [],
+        channel: "BrainStation",
+        image: "http://localhost:8080/Upload-video-preview.jpg",
+        description: req.body.description,
+        views: "0",
+        likes: "0",
+        duration: "0:42",
+        video: "http://localhost:8080/BrainStationSampleVideo.mp4",
+        timestamp: new Date().getTime(),
+    }
+    data.push(video);
+    fs.writeFileSync('./data/database.json', JSON.stringify(data, null, 2));
+    res.status(200).json(video);
+})
 
 
 
